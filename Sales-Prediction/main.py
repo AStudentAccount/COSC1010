2f5aca06ce16232233b208f8b20a87af555ea406
Kellen McBurnett <karatekid507@icloud.com>
2025-02-02 19:48:23 +0000

    Ready for grading

--- Sales-Prediction/main.py
+++ Sales-Prediction/main.py
@@ -1,14 +1,17 @@
 #
-# Name
-# Date
+# Kellen McBurnett
+# Sun, Feb 2
 # Sales Prediction Programming Project
 # COSC 1010
 #
 
 # Variables to hold the sales total and the profit
+#sales total is determined by input
+#profit is sales muliplied by 23% (0.23)
 
 # Get the amount of projected sales.
-
+total_sales = float(input('Enter the projected sales: '))
 # Calculate the projected profit.
-
+profit = total_sales*0.23
 # Print the projected profit.
+print('The profit is $', format(profit, ',.2f'))
