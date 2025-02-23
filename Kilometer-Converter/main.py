cb5fed1c691d2f07631f2a8bd72bf27d851a23d0
Kellen McBurnett <karatekid507@icloud.com>
2025-02-23 19:47:05 +0000

    ready for grading

--- Kilometer-Converter/main.py
+++ Kilometer-Converter/main.py
@@ -1,7 +1,29 @@
 #
-# Name
-# Date
+# Kellen McBurnett
+# Feb 23 2025
 # Kilometer Converter Programming Project
 # COSC 1010
 #
-# Use comments liberally throughout the program. 
+# Set constants.
+CONVERSION_FACTOR = 0.6214
+
+# The main function gets a distance in killometers and calls
+# the show_miles function converts to convert it.
+def main():
+    # Get the number in kilometers.
+    kilometers = float(input('Enter a distance in kilometers: '))
+
+    # Display the distance converted to miles.
+    show_miles(kilometers)
+
+# The show_miles function converts the parameter km from
+# kilmoeter to miles and displays the results.
+def show_miles(km):
+    # Calculate miles.
+    miles = km * CONVERSION_FACTOR
+
+    # Display the miles.
+    print(km, 'kilometers equals', miles, 'miles.')
+
+# Call the main function.
+main()
