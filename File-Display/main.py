ae506760a9eb4c86d7d36faaf2fad3b26e7050ef
Kellen McBurnett <karatekid507@icloud.com>
2025-03-16 19:21:50 +0000

    ready for grading

--- File-Display/main.py
+++ File-Display/main.py
@@ -1,7 +1,16 @@
 #
-# Name
-# Date
+# Kellen McBurnett
+# March 16 
 # File Display Programming Project
 # COSC 1010
 #
-# Use comments liberally throughout the program. 
+# Open the file
+myfile = open('numbers.txt', 'r')
+
+# Read and display file's contents
+for line in myfile:
+    number = int(line)
+    print(number)
+
+# Close the file
+myfile.close()
