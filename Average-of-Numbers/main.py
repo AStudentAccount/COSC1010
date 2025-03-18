736eb33678bdef9d71342d65167e09998452cac1
Kellen McBurnett <karatekid507@icloud.com>
2025-03-18 22:50:13 +0000

    ready for re-grading

--- Average-of-Numbers/main.py
+++ Average-of-Numbers/main.py
@@ -6,23 +6,13 @@
 #
 # Use comments liberally throughout the program. 
 # Open and read the file (given by user input later)
-def file_average(filename):
-    file = open(filename, 'r')
-    number_list = file.readlines
-    # Set total
-    total = 0
-    # Make a loop to go through the number list
-    for number in number_list:
-        # Calculate the numbers as a sum and close the file
-        total = total + float(number)
-    file.close()
-
-# Find the average via division of the total by the number of numbers
-    return total / len(number_list)
-
-# Get the File Name from user input
-input_filename = input("File: ")
-average = file_average(input_filename)
-
+# Open the file, read it
+fileNUM = open('/workspaces/COSC1010/Average-of-Numbers/numbers.txt', 'r')
+# Define numbers as 'nums' and make them a list of ints
+nums = [int(line) for line in fileNUM]
+# Calculate the average
+average = sum(nums) / len(nums)
+# Close the file
+fileNUM.close()
 # Print the average
-print("Average: ", average)
+print (f'The average is {average}')
